07527f00b2f478f2722b36c6f7dd5e5b88adb0f8
Kateryna <79981679+KaterynaTrushko@users.noreply.github.com>
2021-08-12 19:15:04 +0300

    Add files via upload

--- hw_18_cookie/js2.js
+++ hw_18_cookie/js2.js
@@ -1,37 +1,39 @@
-
-function getCookies(str){
-let arr = str.split(';');
-let cookeiObj={};
-arr.forEach(element => {
-  let value = element.split("=");
-  cookeiObj[value[0]] = value[1];
+function getCookies(str) {
+  let arr = str.split(';');
+  let cookeiObj = {};
+  arr.forEach(element => {
+    let value = element.split("=");
+    cookeiObj[value[0]] = value[1];
   });
-return cookeiObj;
+  return cookeiObj;
 }
 
-function addCookies(){
+function addCookies() {
   let valueUser = document.querySelector('.userName').value;
-  document.cookie = 'name=' + valueUser;
-  document.querySelector('.userName').value = '';
-  document.querySelector('.username_form').className = 'hidden';
-  document.querySelector('.welcome').textContent ='Welcome,' + valueUser + '!';
-}
-
-window.onload = function() {
-  let cookies = document.cookie;
-  if(cookies.includes('name')){
-    let cookiesObj = getCookies(cookies);
-    console.log(cookiesObj);
-    let valueUser = cookiesObj[1];
-    document.querySelector('.welcome').textContent ='Welcome, ' + valueUser + '!';
+  if (valueUser) {
+    document.cookie = 'name=' + valueUser;
+    document.querySelector('.userName').value = '';
     document.querySelector('.username_form').className = 'hidden';
-  } else {
-    let btn = document.querySelector('button');
-    btn.addEventListener('click', addCookies())
+    document.querySelector('.welcome').textContent = 'Welcome,' + valueUser + '!';
+  }
+  else{
+    alert('Input Username!');
   }
 }
 
+let btn = document.querySelector('button');
+btn.addEventListener('click', addCookies());
 
-
-
-
+window.onload = function () {
+  let cookies = document.cookie;
+  if (cookies.includes('name')) {
+    let cookiesObj = getCookies(cookies);
+    console.log(cookiesObj);
+    let valueUser = cookiesObj[1];
+    document.querySelector('.welcome').textContent = 'Welcome, ' + valueUser + '!';
+    document.querySelector('.username_form').className = 'hidden';
+  } else {
+    let btn = document.querySelector('button');
+    btn.addEventListener('click', addCookies());
+  }
+}
